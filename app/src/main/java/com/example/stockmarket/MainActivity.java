f66ea5a926463420d8d0344bd13578f87bcaef7c
ariyanSahebghalam <areniussahebius7@gmail.com>
2022-03-17 15:37:49 -0400

    Confusion

--- app/src/main/java/com/example/stockmarket/MainActivity.java
+++ app/src/main/java/com/example/stockmarket/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.stockmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    List stocklist = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        stocklist.add("Intel");
+        stocklist.add("Microsoft");
+        stocklist.add("Apple");
+        stocklist.add("Google");
+        stocklist.add("Facebook");
+        stocklist.add("Disney");
+        stocklist.add("Tesla");
+        stocklist.add("Exxon");
+        stocklist.add("Nvidia");
+        stocklist.add("Qualcomm");
+        stocklist.add("Netflix");
+        stocklist.add("Micron");
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.list_item_layout, stocklist);
+        listView.setAdapter(adapter);
+
+
     }
 }
